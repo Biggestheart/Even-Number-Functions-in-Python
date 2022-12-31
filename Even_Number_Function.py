52e96be6a0366ec7f20aded9bb46ee6812f56730
Anietie Lawrence Hanson Essien <arrhnnie@gmail.com>
2022-12-31 14:17:45 +0100

    Add files via upload

---
+++ Even_Number_Function.py
@@ -0,0 +1,8 @@
+def even(number):
+    if number % 2 == 0:
+        print(str(number) + " is an even number")
+    else:
+        print (str(number) + " is not an even number")
+        
+even(int(input("type a number: ")))
+
